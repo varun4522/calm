699408badfbb5381baf08e95ae51a5324d064dae
ShashankVerma-1234bro <vermashashank070205@gmail.com>
2025-10-14 19:30:16 +0530

    added profile type

---
+++ types/Profile.ts
@@ -0,0 +1,10 @@
+export type Profile = {
+  full_name: string;
+  username: string;
+  group: string;
+  avatar_url?:string;
+  roll_number?: number;
+  professional_title?: string;
+  qualifications?: string;
+  course?:string;
+};
